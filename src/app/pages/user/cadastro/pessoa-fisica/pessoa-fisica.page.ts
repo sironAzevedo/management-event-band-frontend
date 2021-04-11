db2c04bf3b816f0d6efa4e54740b7c2459741a2a
jhonatansoal <jhonatansoal@gmail.com>
2021-04-11 19:51:59 -0300

    alteração jhonatan

--- src/app/pages/user/cadastro/pessoa-fisica/pessoa-fisica.page.ts
+++ src/app/pages/user/cadastro/pessoa-fisica/pessoa-fisica.page.ts
@@ -1,4 +1,8 @@
+import { MenuController, NavController } from '@ionic/angular';
+import { UserService } from './../../../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'app-pessoa-fisica',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pessoa-fisica.page.scss'],
 })
 export class PessoaFisicaPage implements OnInit {
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    public menu: MenuController,
+    public router: NavController,
+    public formBuilder: FormBuilder,
+    public userService: UserService,
+    private ionLoader: LoaderService
+  ) { }
 
   ngOnInit() {
+    this.validFields();
+  }
+  
+  validFields() {
+    this.formGroup = this.formBuilder.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])],
+      senha: ['', Validators.required]
+    });
   }
 
 }
